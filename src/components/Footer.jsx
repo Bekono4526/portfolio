641b5886941fa4b7ae81b24afc5105806272b05e
Bekono4526 <bekonorosy0@gmail.com>
2024-07-16 17:09:36 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <p>Copyright &copy; 2024 Tous droits réservés.</p>
+    <footer className="bg-dark-purple text-white py-4">
+      <p className="text-center">Copyright &copy; 2024 Tous droits réservés.</p>
     </footer>
   );
 }
