bf384bc63c438492954d351312a25774bfb81d58
Bekono4526 <bekonorosy0@gmail.com>
2024-07-16 23:25:11 +0200

    cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,16 @@ import React from "react";
 function Card({ title, description, image }) {
     return (
         <div className="relative flex w-80 flex-col rounded-xl bg-white shadow-md">
-            <div className="h-40 overflow-hidden rounded-xl bg-gradient-to-r from-blue-500 to-blue-600"></div>
+            {/* Image container with a conditional rendering of a background image or a default gradient */}
+            <div
+                className="h-40 overflow-hidden rounded-xl"
+                style={{ backgroundImage: image ? `url(${image})` : 'none', backgroundSize: 'cover', backgroundPosition: 'center' }}
+            >
+                {!image && <div className="w-full h-full bg-gradient-to-r from-blue-500 to-blue-600"></div>}
+            </div>
             <div className="p-6">
                 <h5 className="mb-2 text-xl font-semibold leading-snug text-dark-purple truncate">
-                    {title || 'Tailwind Card'} {/* Tronquer le titre s'il est trop long */}
+                    {title || 'Tailwind Card'}
                 </h5>
                 <p className="text-base leading-relaxed text-gray-700 overflow-hidden text-ellipsis">
                     {description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.'}
